63bedb45683f43461148bd97aeb4261983c46ac9
FelipeF-soares <felipe.fends@gmail.com>
2025-01-07 12:14:06 -0300

    2. Added items to the Category table in the database and created the Controller and Views for the Category.

---
+++ BookStoreWeb/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using BookStoreWeb.Data;
+using BookStoreWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreWeb.Controllers;
+
+public class CategoryController : Controller
+{
+    private readonly ApplicationDbContext context;
+
+    public CategoryController(ApplicationDbContext context)
+    {
+        this.context = context;
+    }
+    public IActionResult Index()
+    {
+       var listCategory = context.Categories.ToList();
+        return View(listCategory);
+    }
+}
